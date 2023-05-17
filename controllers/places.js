5936adc3789c53923f076cb04fd2548d463b29f0
angelcepeda <jangelcepeda@protonmail.com>
2023-05-16 17:40:23 -0700

    controllers updated

--- controllers/places.js
+++ controllers/places.js
@@ -125,17 +125,108 @@ function destroy(req,res,next){
 }
 
 function replace(req,res,next){
+    const id = req.params.id;
+    let name = req.body.name ? req.body.name:"";
+    let description = req.body.name ? req.body.name:"";
+    let country = req.body.country ? req.body.country:"";
+    let city = req.body.city ? req.body.city:"";
+    let street = req.body.street ? req.body.street:"";
+    let number = req.body.number ? req.body.number:"";
+    let interior = req.body.interior ? req.body.interior:"";
+    let zip = req.body.zip ? req.body.zip:"";
+    let latitud = req.body.latitud ? req.body.latitud:"";
+    let longitud = req.body.longitud ? req.body.longitud:"";
 
+    let place = new Place({
+        _name:name,
+        _description:description,
+        _country:country,
+        _city:city,
+        _street:street,
+        _number:number,
+        _interior:interior,
+        _zip:zip,
+        _latitud:latitud,
+        _longitud:longitud
+    });
+
+    place.findOneAndUpdate({"_id": id}, institution, {new: true})
+            .then(obj => {res.status(200).json({
+                message: "Place updated",
+                obj:obj
+            })}).catch(ex => res.status(500).json({
+                message:"Place not updated",
+                err:ex
+            }));
 }
 
 function update(req,res,next){
+    const id = req.params.id;
+    let name = req.body.name ? req.body.name:"";
+    let description = req.body.name ? req.body.name:"";
+    let country = req.body.country ? req.body.country:"";
+    let city = req.body.city ? req.body.city:"";
+    let street = req.body.street ? req.body.street:"";
+    let number = req.body.number ? req.body.number:"";
+    let interior = req.body.interior ? req.body.interior:"";
+    let zip = req.body.zip ? req.body.zip:"";
+    let latitud = req.body.latitud ? req.body.latitud:"";
+    let longitud = req.body.longitud ? req.body.longitud:"";
 
+    let place = new Object();
+
+    if(name){
+        place._name = name;
+    }
+    if(description){
+        place._description = description;
+    }
+    if(country){
+        place._country = country;
+    }
+    if(city){
+        place._city = city;
+    }
+    if(street){
+        place._street = street;
+    }
+    if(number){
+        place._number = number;
+    }
+    if(interior){
+        place._interior = interior;
+    }
+    if(zip){
+        place._zip = zip;
+    }
+    if(latitud){
+        place._latitud = latitud;
+    }
+    if(longitud){
+        place._longitud = longitud;
+    }
+
+    Place.findOneAndUpdate({"_id": id}, place)
+            .then(obj => res.status(200).json({
+                message: "Place updated",
+                obj:obj
+            })).catch(ex => res.status(500).json({
+                message:"Place not updated",
+                err:ex
+            }));
 }
 
 function destroy(req,res,next){
-
+    const id = req.params.id;
+    Place.findOneAndRemove({"_id": id})
+            .then(obj => res.status(200).json({
+                message: "Place deleted",
+                obj:obj
+            })).catch(ex => res.status(500).json({
+                message:"Place not deleted",
+                err:ex
+            }));
 }
-
 function getMapsCoordenates(req,res,next){
 
 }
